88855e4d463294d3adcf0e7314967389438e2b81
Jai Paton <jamespaton029@gmail.com>
2025-04-25 10:20:26 +0100

    Made Timer work for clown stall

--- p010621n-VR/Assets/Scripts/WaterGun.cs
+++ p010621n-VR/Assets/Scripts/WaterGun.cs
@@ -1,18 +1,28 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class WaterGun : MonoBehaviour
 {
     [SerializeField] GameObject m_WaterBullet;
     [SerializeField] GameObject m_FireTransform;
     Quaternion startRot;
+    XRGrabInteractable m_GrabInteractable;
+    public ClownManager m_ClownManager;
 
     float bulletSpeed = 10.0f;
     bool Firing = false;
+
+    bool m_IsActive = false;
+    float m_Timer = 120.0f;
+    [SerializeField] TextMeshProUGUI m_Text;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startRot = gameObject.transform.rotation;
+        m_GrabInteractable = GetComponent<XRGrabInteractable>();
     }
 
     // Update is called once per frame
@@ -26,12 +36,44 @@ public class WaterGun : MonoBehaviour
                 BulletSpawn.GetComponent<Rigidbody>().AddForce(m_FireTransform.transform.up * bulletSpeed, ForceMode.Impulse);
             }
         }
+
+        if (m_IsActive)
+        {
+            m_Timer -= Time.deltaTime;
+        }
+
+        if (m_Timer <= 0)
+        {
+            m_GrabInteractable.enabled = false;
+            ResetGrab();
+            ResetRot();
+            m_IsActive = false;
+            m_Timer = 120.0f;
+            m_ClownManager.ResetClowns();
+        }
+
+
+        //update timer text
+        float minutes = m_Timer / 60;
+        float seconds = m_Timer % 60;
+        if (seconds < 10.0f)
+        {
+            seconds = '0' + seconds;
+        }
+        m_Text.text = minutes + ":" + seconds;
     }
 
+    IEnumerator ResetGrab()
+    {
+        yield return new WaitForSeconds(1);
+
+        m_GrabInteractable.enabled = true;
+    }
     
     public void StartFiring()
     {
         Firing = true;
+        m_IsActive = true;
     }
 
     public void StopFiring()
